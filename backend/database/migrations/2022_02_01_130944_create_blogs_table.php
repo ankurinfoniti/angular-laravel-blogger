4e7a84524df4a9283ea695e96f72c84c6f051bce
ankurinfoniti <ankur.infoniti@gmail.com>
2022-02-01 18:52:21 +0530

    migration for database

--- backend/database/migrations/2022_02_01_130944_create_blogs_table.php
+++ backend/database/migrations/2022_02_01_130944_create_blogs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateBlogsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('blogs', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->tinyInteger('role')->default(0);
+            $table->string('title')->nullable();
+            $table->integer('user_id');
+            $table->integer('category_id')->nullable();
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->tinyInteger('is_featured')->default(0);
             $table->tinyInteger('is_active')->default(0);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -33,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('blogs');
     }
 }
